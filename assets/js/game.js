c3c335214b9cece544270694dff7b7d89114d482
Asher Clawson <31211822+KingPogona@users.noreply.github.com>
2020-08-10 17:42:13 -0600

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -29,15 +29,18 @@ var fight = function(enemyName) {
             if (confirmSkip) {
                 window.alert(playerName + ' has decided to skip this fight. Goodbye!');
                 // subtract money from playerMoney for skipping
-                playerMoney = playerMoney - 10;
+                playerMoney = Math.max(0, playerMoney - 10);
                 console.log("playerMoney", playerMoney)
                 break;
             }
         }
     
         // remove enemy's health by subtracting the amount set in the playerAttack variable
-        enemyHealth = enemyHealth - playerAttack;
-        console.log(playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.');
+        // generate random damage value based on player's attack power
+        var damage = randomNumber(playerAttack - 3, playerAttack);
+
+        enemyHealth = Math.max(0, enemyHealth - damage);
+        window.alert(playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.');
     
         // check enemy's health
         if (enemyHealth <= 0) {
@@ -50,12 +53,15 @@ var fight = function(enemyName) {
             break;
         }   
         else {
-            window.alert(enemyName + ' still has ' + enemyHealth + ' health left.');
+            //window.alert(enemyName + ' still has ' + enemyHealth + ' health left.');
         }
     
         // remove players's health by subtracting the amount set in the enemyAttack variable
-        playerHealth = playerHealth - enemyAttack;
-        console.log(enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.');
+        // generate random damage value based on enemy's attack power
+        var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+        playerHealth = Math.max(0, playerHealth - damage);
+        window.alert(enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.');
     
         // check player's health
         if (playerHealth <= 0) {
@@ -64,7 +70,7 @@ var fight = function(enemyName) {
             break;
         } 
         else {
-            window.alert(playerName + ' still has ' + playerHealth + ' health left.');
+            //window.alert(playerName + ' still has ' + playerHealth + ' health left.');
         }
     }
 };
@@ -86,7 +92,7 @@ var startGame = function() {
             var pickedEnemyName = enemyNames[i];
         
             // reset enemyHealth before starting new fight
-            enemyHealth = 50;
+            enemyHealth = randomNumber(40, 60);
         
             // use debugger to pause script from running and check what's going on at that moment in the code
             // debugger;
@@ -192,5 +198,16 @@ var shop = function() {
             break;
     }
 };
+
+
+// function to generate a random numeric value
+var randomNumber = function(min, max) {
+    var value = Math.floor(Math.random() * (max - min + 1) + min);
+  
+    return value;
+};
+
+
+
 
 startGame();
